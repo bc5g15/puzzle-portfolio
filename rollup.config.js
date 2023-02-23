dc6f9e895f0fa5e89e3eb585f758ca6a81cfc4aa
Benedict Cairns <bcairns21@gmail.com>
2023-02-23 10:44:18 +0000

    Correct deployment location

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import typescript from "@rollup/plugin-typescript";
 export default {
     input: 'src/main.ts',
     output: {
-        file: './output/bundle.js',
+        file: './public/bundle.js',
         format: 'iife'
     },
     plugins: [typescript({ lib: ["es5", "es6", "dom", "es2018"], target: "es6"})]
